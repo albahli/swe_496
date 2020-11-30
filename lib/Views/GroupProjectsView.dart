8c92cf605ac1b7f7ae55fa00cf469c55dedbf187
Faisal <faisalbahli@gmail.com>
2020-11-30 20:24:47 +0300

    Added search bar for list of projects in 'Group Projects View'

--- lib/Views/GroupProjectsView.dart
+++ lib/Views/GroupProjectsView.dart
@@ -11,6 +11,7 @@ import 'package:swe496/controllers/ProjectControllers/ListOfProjectsContoller.da
 import 'package:swe496/controllers/ProjectControllers/projectController.dart';
 import 'package:swe496/controllers/UserControllers/authController.dart';
 import 'package:swe496/controllers/UserControllers/userController.dart';
+import 'package:swe496/models/Project.dart';
 import 'package:timeline_list/timeline.dart';
 import 'package:timeline_list/timeline_model.dart';
 import 'AccountSettings.dart';
@@ -27,6 +28,9 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
   final TextEditingController _newProjectNameController =
       TextEditingController();
   int barIndex = 0;
+
+  String keyword = '';
+  List<Project> filteredProjectsListBySearch = new List<Project>();
 
   @override
   Widget build(BuildContext context) {
@@ -70,6 +74,7 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
                 content: Text("My Profile"),
                 onClick: () {
                   Get.to(AccountSettings());
+                  FocusScope.of(context).unfocus();
                 }),
             MLMenuItem(
               leading: Icon(
@@ -111,14 +116,15 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
       child: TextField(
         decoration: InputDecoration(hintText: 'Search'),
         onChanged: (textVal) {
-          textVal = textVal.toLowerCase();
+          setState(() {
+            keyword = textVal;
+          });
         },
       ),
     );
   }
 
   Widget getListOfProjects() {
-
     return Expanded(
       child: GetX<ListOfProjectsController>(
           init: Get.put<ListOfProjectsController>(ListOfProjectsController()),
@@ -127,26 +133,34 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
                 listOfProjectsController.projects != null &&
                 !listOfProjectsController.projects.isNullOrBlank &&
                 !listOfProjectsController.projects.isNull &&
-                listOfProjectsController.projects.length != 0) {
+                listOfProjectsController.projects.length != 0 &&
+                listOfProjectsController.projects.isNotEmpty) {
+              filteredProjectsListBySearch = listOfProjectsController.projects
+                  .where((project) => project.projectName
+                      .toLowerCase()
+                      .contains(keyword.toLowerCase()))
+                  .toList();
+
               return ListView.builder(
-                  itemCount: listOfProjectsController.projects.length,
+                  itemCount: filteredProjectsListBySearch.length,
                   itemBuilder: (context, index) {
                     return ListTile(
                       leading: Icon(Icons.supervised_user_circle),
-                      title: Text(
-                          listOfProjectsController.projects[index].projectName),
+                      title:
+                          Text(filteredProjectsListBySearch[index].projectName),
                       subtitle: Text('Details...'),
                       onTap: () async {
                         Get.put<ProjectController>(ProjectController(
-                            projectID: listOfProjectsController
-                                .projects[index].projectID));
+                            projectID:
+                                filteredProjectsListBySearch[index].projectID));
                         // sleep(Duration(milliseconds:600));
                         ProjectController projectController =
                             Get.find<ProjectController>();
-                         if (projectController.initialized) {
+                        if (projectController.initialized) {
                           Get.to(TasksAndEventsView(),
                               transition: Transition.rightToLeft,
                               duration: Duration(milliseconds: 300));
+                          FocusScope.of(context).unfocus();
                         }
                       },
                     );
@@ -194,7 +208,7 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
             return;
           else if (barIndex == 2)
             Get.off(FriendsView(), transition: Transition.noTransition);
-          else if (barIndex ==3)
+          else if (barIndex == 3)
             Get.off(MessagesView(), transition: Transition.noTransition);
         });
 
@@ -256,16 +270,19 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
             size: 25,
           ),
           label: 'Join Project',
-          onTap: () => print('SECOND CHILD')  ,
+          onTap: () => print('SECOND CHILD'),
         ),
         SpeedDialChild(
-          child: Icon(
-            Icons.add,
-            size: 25,
-          ),
-          label: 'New Project ',
-          onTap: () =>  Get.to(CreateProjectView())//alertCreateProjectForm(context),
-        ),
+            child: Icon(
+              Icons.add,
+              size: 25,
+            ),
+            label: 'New Project ',
+            onTap: () {
+                Get.to(CreateProjectView());
+                FocusScope.of(context).unfocus();
+            }
+            ),
       ],
     );
   }
@@ -463,7 +480,7 @@ class _GroupProjectsViewState extends State<GroupProjectsView> {
               formKey.currentState.save();
               if (formKey.currentState.validate()) {
                 try {
-                //  ProjectCollection().createNewProject(
+                  //  ProjectCollection().createNewProject(
                   //    _newProjectNameController.text, userController.user);
                   Get.back();
                   // Display success message
