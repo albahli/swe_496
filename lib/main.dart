08fd9fd435640f4db86f3d2a6b1a548af5aaef56
Faisal <faisalbahli@gmail.com>
2020-12-10 08:06:57 +0300

    notification testing.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:swe496/controllers/bindings/AuthBinding.dart';
 import 'package:get/get.dart';
-import 'file:///C:/Users/faisa/AndroidStudioProjects/swe_496/lib/utils/Notificatoins/NotificationService.dart';
+import 'package:swe496/utils/Notificatoins/NotificationService.dart';
 import 'package:swe496/utils/root.dart';
 
 void main() => runApp(MyApp());
